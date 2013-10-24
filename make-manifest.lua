bb48445e9395807052a6d191bfb8fa602f0a92ee
Clement Farabet <clement.farabet@gmail.com>
2013-10-24 12:50:26 -0400

    New script to generate proper templates.

---
+++ make-manifest.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env luajit
+
+require('pl.text').format_operator()
+
+os.execute[[
+git pull
+luarocks-admin make_manifest .
+]]
+
+content = io.open('index.html'):read('*all')
+_,_,table = content:find('(%<table.*%<%/table%>)')
+
+template = io.open('template.html'):read('*all')
+result = template % {rocks = table}
+
+io.open('index.html','w'):write(result)
